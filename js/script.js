a147871da0608813d1c7a7ab8d0bfd91bb54c89b
Dylan Kocsis <Dylan.Kocsis@queensu.ca>
2017-10-20 16:22:32 -0700

    images appear

--- js/script.js
+++ js/script.js
@@ -1,27 +1,29 @@
 $(document).ready(function() {
+  
+    $('#sections').on('change',function(){
+      var section = $('#sections').val();
+      var url = 'https://api.nytimes.com/svc/topstories/v2/' + section + '.json';
+      url += '?' + $.param({'api-key': '516e14d87c8e478195abbd4aaa72b938'
+  });
+  
+$('#stories ul li').remove();
 
-  $('#sections').on('change',function(){
-    var section = $('#sections').val();
-    var url = 'https://api.nytimes.com/svc/topstories/v2/' + section + '.json';
-    url += '?' + $.param({'api-key': '516e14d87c8e478195abbd4aaa72b938'
-});
+  $.ajax({
+    url: url,
+    method: 'GET'
+  }).done(function(data) {
+    
+    var slicedData = data.results.slice(0,12);
+    console.log(slicedData);
+  $.each(slicedData, function(index, value) {
+    $('#stories > ul').append('<li>' + value.abstract + '<div class="imageurl" style="background-image:url(' + value.multimedia[4].url + ')"> </div></li>');
+  });
+  
 
-$.ajax({
-  url: url,
-  method: 'GET'
-}).done(function(data) {
-  console.log(data);
-
-$.each(data.results, function(index, value) {
-  console.log(value.title);
-  $('#stories > ul').append('<li>' + value.abstract + '</li>');
-  $('li').slice(12).css('display', 'none');
-});
-
-})
-.fail(function(err) {
-  throw err;
-}); 
-}); 
-})
-//Hints: Will need to use .Filter() and .Slice()
+  })
+  .fail(function(err) {
+    throw err;
+  }); 
+  }); 
+  })
+  //Hints: Will need to use .Filter() and .Slice()
